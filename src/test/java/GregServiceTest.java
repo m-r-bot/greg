5c1b6c69e2457e856bc532722470b7a6b7daa051
Maria <mariaboe81@gmail.com>
2022-11-03 16:14:34 +0100

    Tests for Holiday in federal and other States method

--- src/test/java/GregServiceTest.java
+++ src/test/java/GregServiceTest.java
@@ -1,2 +1,27 @@
+import org.example.GregService;
+import org.example.types.FederalState;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
 public class GregServiceTest {
+
+    GregService gregService = new GregService(FederalState.SN);
+
+        @Test
+        public void testIsHolidayInState(){
+            var currentDay =LocalDate.of(2022, 3, 1);
+            boolean isHolidayInState = gregService.isHolidayInCurrentFederalState(currentDay);
+
+            Assert.assertFalse(isHolidayInState);
+        }
+
+    @Test
+    public void testIsHolidayInOtherState(){
+          var currentDay =LocalDate.of(2022, 3, 1);
+        boolean isHolidayInOtherState = gregService.isHolidayInOtherFederalState(currentDay);
+
+        Assert.assertTrue(isHolidayInOtherState);
+    }
 }
