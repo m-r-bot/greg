17c4c59631cd23ba71a73967c8cbdf3a21a7fde0
Maria <mariaboe81@gmail.com>
2022-12-01 18:45:03 +0100

    tried around with replacing string method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import com.sun.xml.bind.AnyTypeAdapter;
 import org.example.types.FederalState;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Locale;
 
 public class Main {
@@ -18,6 +18,27 @@ public class Main {
         //input values for desired year and federal state
         SvgCalendar calendar = GregService.getGreg(2022,FederalState.SN);
         GregService.writeCalendarAsSvg(calendar, "calendar-v06");
+
+        File file = new File("C:/Users/mboese/Desktop/Dev/greg/calendar-v06.svg");
+        //calendar.toString().replace("&lt;" , "<").replace("&rt", ">");
+       try {
+           BufferedReader br = new BufferedReader(new FileReader(file));
+
+           String line;
+           while ((line = br.readLine()) != null) {
+               String str = line.replace("&lt;", "<").replace("&rt", ">");
+               System.out.println(str);
+
+               BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+               bw.write(str);
+               bw.close();
+           }
+           br.close();
+
+       } catch (IOException e){
+           e.printStackTrace();
+       }
+
     }
 
 }
