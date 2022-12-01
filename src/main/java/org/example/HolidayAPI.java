27269fc5d2fc7a2bf293462ac0191baaa5fbd6ac
Maria <mariaboe81@gmail.com>
2022-12-01 18:43:39 +0100

    created beginning of API accessor

---
+++ src/main/java/org/example/HolidayAPI.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.io.IOException;
+import java.net.http.HttpRequest;
+
+public class HolidayAPI {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpRequest getRequest = HttpRequest.newBuilder().build();
+
+    }
+}
